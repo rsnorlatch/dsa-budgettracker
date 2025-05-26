2d42bec1da162c23f1be609943e56a680872feef
rsnorlatch <muhfarisnew@gmail.com>
2025-05-26 16:59:55 +0700

    added node deletion by memory address

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ Node *node__tambah(T data, Node *next) {
   return node_baru;
 }
 
+// NOTE: WAJIB DIPANGGIL DIAKHIR PROGRAM
 void node__free(Node *head) {
   Node *current = head;
 
@@ -47,11 +48,35 @@ void node__print_T(Node *head) {
   } 
 
   cout << "NULL" << endl;
-  node__free(head);
 }
 
 void node__sisip_depan(Node *&head, T data) {
   head = node__tambah(data, head);
+}
+
+void node__hapus_berdasarkan_alamat(Node *&head, Node *target) {
+  if (target == nullptr) {
+    cout << "[ERROR] anda mencoba menghapus nullpointer!!!" << endl;
+    return;
+  }
+
+  Node *current = head;
+
+  if (current == target) {
+    head = current->next;
+    delete current;
+    return;
+  }
+
+  while (current != nullptr) {
+    if (current->next != target) {
+      current = current->next;
+      continue;
+    }
+
+    current->next = current->next->next;
+    delete target;
+  }
 }
 
 int main() {
@@ -61,4 +86,7 @@ int main() {
   node__sisip_depan(n, 4);
 
   node__print_T(n);
+  node__hapus_berdasarkan_alamat(n, n);
+  node__print_T(n);
+  node__free(n);
 }
