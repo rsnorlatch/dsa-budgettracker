b921a04bfff9d071341a84c1cb47dfaa2d79df7b
rsnorlatch <muhfarisnew@gmail.com>
2025-05-26 16:31:59 +0700

    removed file function because it's not related to branch

--- main.cpp
+++ main.cpp
@@ -52,30 +52,6 @@ void node__print_T(Node *head) {
 
 
 
-
-void file__simpan_target_bulanan(int *target_bulanan) {
-  FILE *file_target_bulanan = fopen("./store/target_bulanan.bin", "wb");
-
-  fwrite(target_bulanan, sizeof(int), 1, file_target_bulanan);
-
-  fclose(file_target_bulanan);
-}
-
-void file__baca_target_bulanan(int *target_bulanan) {
-  FILE *file_target_bulanan = fopen("./store/target_bulanan.bin", "rb");
-  fread(target_bulanan, sizeof(int), 1, file_target_bulanan);
-
-  fclose(file_target_bulanan);
-}
-
-void file__reset_target_bulanan(int *target_bulanan) {
-  FILE *file_target_bulanan = fopen("./store/target_bulanan.bin", "wb");
-  int overwrite_value = 0;
-  fwrite(&overwrite_value, sizeof(int), 1, file_target_bulanan);
-
-  fclose(file_target_bulanan);
-}
-
 int main() {
   Node *n = node__tambah(1, node__tambah(2, nullptr));
 
