b5a9cb5a866174cb065d36355e8c3c25edf9c15b
rsnorlatch <muhfarisnew@gmail.com>
2025-05-26 15:52:38 +0700

    added file function to write montly budget to a file

--- main.cpp
+++ main.cpp
@@ -8,10 +8,28 @@ struct Entry {
   int nominal;
 };
 
+void file__simpan_target_bulanan(int *target_bulanan) {
+  FILE *file_target_bulanan = fopen("./store/target_bulanan.bin", "wb");
+
+  fwrite(target_bulanan, sizeof(int), 1, file_target_bulanan);
+
+  fclose(file_target_bulanan);
+}
+
+void file__baca_target_bulanan(int *target_bulanan) {
+  FILE *file_target_bulanan = fopen("./store/target_bulanan.bin", "rb");
+  fread(target_bulanan, sizeof(int), 1, file_target_bulanan);
+
+  fclose(file_target_bulanan);
+}
+
+void file__reset_target_bulanan(int *target_bulanan) {
+  FILE *file_target_bulanan = fopen("./store/target_bulanan.bin", "wb");
+  int overwrite_value = 0;
+  fwrite(&overwrite_value, sizeof(int), 1, file_target_bulanan);
+
+  fclose(file_target_bulanan);
+}
 
 int main() {
-  int *p = (int *)malloc(sizeof(int));
-  *p = 10;
-
-  cout << *p << endl;
 }
